12f9dbed77d8c83e6f3b39d414b6cf46a3453bb4
Pullock4981 <pullock15-4981@diu.edu.bd>
2024-02-16 11:54:48 +0600

    add js file and logical implement

--- alpha.js
+++ alpha.js
@@ -13,7 +13,50 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
-function play(){
+function handleKeyBtnPress(event) {
+    const playerPressed = event.key;
+    console.log('Player pressed', playerPressed);
+
+    const currentAlphaElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphaElement.innerText.toLowerCase();
+    // const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(currentAlphabet, playerPressed);
+    // console.log(currentAlphaElement.innerText);
+
+    // check
+    if (playerPressed === currentAlphabet) {
+        console.log('you get a point');
+        console.log('you have pressed currently:', currentAlphabet);
+        removeBgColorById(currentAlphabet);
+        continueGame();
+    }
+    else {
+        console.log('you missed a life');
+    }
+}
+// key press
+document.addEventListener('keyup', handleKeyBtnPress);
+
+function continueGame() {
+
+    const alphabet = getARandomAlphabet();
+    console.log(alphabet);
+
+    // random alpha display
+
+    const currentAlphaElement = document.getElementById('current-alphabet');
+    currentAlphaElement.innerText = alphabet;
+
+    // set bg color
+
+    addBgColorById(alphabet)
+}
+
+
+function play() {
     hideElementById('home');
     showElementById('play-ground');
+    continueGame();
 }
+
+
--- utility.js
+++ utility.js
@@ -1,9 +1,34 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function addBgColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-500');
+}
+
+function removeBgColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-500');
+}
+
+function getARandomAlphabet() {
+    const alphabetString = 'abcdefghijklmnopqrstuvwyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    
+    // console.log(index, alphabet);
+    return alphabet
+}
+
